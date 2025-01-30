133ca776735301b752c479c19264fe767dce88aa
AlmightyPeanut <benedikthornig@outlook.com>
2025-01-30 19:51:10 +0100

    simple main experiment loop

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # add different strategies for debate?
 # - never admit defeat (Trump tactic)
 # - make stuff up about the original text
+from Debate import Debate
 from load_data import Dataset
 
 # investigate without the quote system
@@ -11,3 +12,8 @@ if __name__ == '__main__':
     data = Dataset()
     for article, question, correct_answer, false_answer in data:
         pass
+    for article, question, correct_answer, false_answer, question_id in data:
+        debate = Debate(question_id=question_id, story=article, question=question, correct_answer=correct_answer,
+                        false_answer=false_answer)
+        debate.start(use_quote_verification=True)
+        debate.start(use_quote_verification=False)
